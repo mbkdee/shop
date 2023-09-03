050b9e68c0aa2632b1b0243ed603569e634a530f
YofZainbinia <125333452+mbkdee@users.noreply.github.com>
2023-09-03 19:06:07 +0200

    Create config.php

---
+++ config/config.php
@@ -0,0 +1,20 @@
+<?php
+	class config
+	{
+	    public $bot_id             = '404708889:AAGUE6Qt_kMSGPlCsHjVFS61xcq7VTY-vuM';
+		public $MerchantID         = '86597dcc-5713-11e7-9f8b-005056a205be';// MID or MerchantID ZARIN PAL
+		public $path               = 'https://sajadhajirezaee.ir/Customer/foroshMember/';
+		public $database_host      = 'localhost';
+		public $database_name      = 'sajadhaj_foroshMember';
+		public $database_username  = 'sajadhaj_hossein';
+		public $database_password  = 'khcEmGfqJ)va';
+		public $bot_Username       = 'RoobotTestBot';
+		public $admin_list         = array(113271074,332849804,112749559);//admin id
+		public $adminSend          = 113271074;
+		public $version            = "1.0";
+		public $NameBOT            = 'فروش عضو';
+		public $CHANNEL_NAME       = 'فروش عضو';
+		public $Description        = 'خرید عضو ';// Description IN PAYMENT PAGE
+		public $CHANNEL_ID         = '@Source_Home';// CHANNEL ID FOR EXP. @Source_Home OR -100531321224
+		public $CHANNEL_LINK       = 'https://t.me/Source_Home';// FOR EXP. https://t.me/Source_Home OR https://t.me/Source_Home
+	} 
