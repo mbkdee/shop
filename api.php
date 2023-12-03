975f4451f26fb12069535ebab110baea4eceafd1
YofZainbinia <125333452+mbkdee@users.noreply.github.com>
2023-12-03 03:19:25 +0100

    Update api.php

--- api.php
+++ api.php
@@ -34,7 +34,7 @@ $chat_id = 'TARGET_CHAT_ID';
 
 <?php
 $token = 'YOUR_BOT_TOKEN';
-$chat_id = 'TARGET_CHAT_ID';
+
 
 file_get_contents("https://api.telegram.org/bot$token/deleteMessage?chat_id=$chat_id&message_id=$message_id");
 ?>
